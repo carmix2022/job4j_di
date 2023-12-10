65d1fb06e6d0b09e3d88db135c9cb8949972ca24
Илья <ily470@yandex.ru>
2023-12-10 21:06:18 +0700

    0. Что такое DI? [#6876 #465651]

--- src/main/java/annotation/Dog.java
+++ src/main/java/annotation/Dog.java
@@ -2,11 +2,13 @@ package annotation;
 
 public class Dog implements Pet<Dog> {
     private String name = "Dog";
-    public Dog() {}
+
+    public Dog() { }
 
     public Dog(String name) {
         this.name = name;
     }
+
     @Override
     public String print() {
         return "Dog name = %s".formatted(name);
